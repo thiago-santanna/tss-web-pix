7758b2c84a9a0a0e7e42144104bb6b25cbc5f3e2
xthiagosant <uzmaki_thiago@outlook.com>
2021-09-02 13:51:27 -0300

    mudanças testes no hello

--- src/services/hello/index.js
+++ src/services/hello/index.js
@@ -1,5 +1,5 @@
-const hello = "Hello"
-const me = "Prazer sou Thiago"
+const hello = "Hello, Sou uma API paramitir PIX."
+const me = "Prazer sou Thiago o desenvolvedor."
 
 // Só que aqui irei exportar os retornos de funções que irão
 // se comunicar com a api do itau
